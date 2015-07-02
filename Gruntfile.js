b1f17a8f1c5670cad52bc37ea49fc0f56b05a657
Tino Rusch <tino.rusch@webvariants.de>
2015-07-02 12:10:11 +0200

    [clientjs] removed client specific js files (they were not finished -> useless);

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,22 @@
 module.exports = function(grunt) {
 
-  var jsFiles = ['Gruntfile.js',
-                       'js/lib/Model.js',
-                       'js/lib/InstanceBuilder.js',
-                       'js/lib/ServerModel.js',
-                       'js/lib/ClientModel.js',
-                       'js/models/**/*.js',
-                       'test/**/*.js',
-                       'js/pages/**/*.js'];
+  var jsFiles = [
+     'js/lib/Aggregation.js',
+     'js/lib/Model.js',
+     'js/lib/ServerModel.js',
+     'js/lib/ServerApp.js',
+     'js/models/**/common.js',
+     'js/models/**/server.js',
+     'js/pages/**/*.js'
+  ];
 
   grunt.initConfig({
     concat: {
       options: {
         stripBanners: true
       },
-      clientjs: {
-        src: [
-           'js/lib/Aggregation.js',
-           'js/lib/Model.js',
-           'js/lib/ClientModel.js',
-           'js/lib/ClientApp.js',
-           'js/models/**/common.js',
-           'js/models/**/client.js'
-        ],
-        dest: 'bin/assets/client.js'
-      },
       serverjs: {
-        src: [
-           'js/lib/Aggregation.js',
-           'js/lib/Model.js',
-           'js/lib/ServerModel.js',
-           'js/lib/ServerApp.js',
-           'js/models/**/common.js',
-           'js/models/**/server.js',
-           'js/pages/**/*.js'
-        ],
+        src: jsFiles,
         dest: 'bin/server.js'
       }
     },
@@ -43,35 +25,19 @@ module.exports = function(grunt) {
         sourceMaps: true,
         comments: false
       },
-      clientjs: {
-        src: 'bin/assets/client.js',
-        dest: 'bin/assets/client.js'
-      },
       serverjs: {
         src: 'bin/server.js',
         dest: 'bin/server.js'
       }
     },
-    jshint: {
-      files: jsFiles,
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
     watch: {
       js: {
         files: jsFiles,
-        tasks: ['jshint', 'buildjs']
+        tasks: ['buildjs','start']
       },
       go: {
         files: ['server/**/*.go'],
-        tasks: ['shell:stop','buildgo','shell:start']
+        tasks: ['buildgo','start']
       }
     },
     shell: {
@@ -87,7 +53,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-shell');
@@ -95,7 +60,7 @@ module.exports = function(grunt) {
 
 
   grunt.registerTask('start',['shell:stop','shell:start']);
-  grunt.registerTask('buildjs',['concat:clientjs','concat:serverjs','babel:clientjs','babel:serverjs']);
+  grunt.registerTask('buildjs',['concat:serverjs','babel:serverjs']);
   grunt.registerTask('buildgo',['shell:gobuild']);
   grunt.registerTask('build',['buildgo','buildjs']);
   grunt.registerTask('default', ['build','watch']);

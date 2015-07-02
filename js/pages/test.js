c045fd09969aaf7a5dfa289c8bf241c247da6574
Tino Rusch <tino.rusch@webvariants.de>
2015-07-02 12:40:35 +0200

    [testjs] added sample event emit on /test visit;

--- js/pages/test.js
+++ js/pages/test.js
@@ -3,6 +3,8 @@ restless.Page('^/test$', ()=>{
     let apple = restless.CreateModel('Apple');
     apple.getFromUID('27fdd834-d15c-46b6-af0c-606ed0a58d01')
 
+    events.emit('test-visit@websocket')
+
     return {
         body: `
 Url: ${URL}
